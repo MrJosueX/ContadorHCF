624119f635ef5f5e95ec5177ee4838457446ed00
MrJosueX <58127677+MrJosueX@users.noreply.github.com>
2019-11-23 22:29:57 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Contador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnMassMove = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnWarn = new System.Windows.Forms.Button();
+            this.btnMute = new System.Windows.Forms.Button();
+            this.btnKick = new System.Windows.Forms.Button();
+            this.btnBan = new System.Windows.Forms.Button();
+            this.btnSS = new System.Windows.Forms.Button();
+            this.btnUnban = new System.Windows.Forms.Button();
+            this.lblAgregar = new System.Windows.Forms.Label();
+            this.txtContador = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInicializador = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnTp = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMassMove
+            // 
+            this.btnMassMove.Location = new System.Drawing.Point(38, 160);
+            this.btnMassMove.Name = "btnMassMove";
+            this.btnMassMove.Size = new System.Drawing.Size(75, 23);
+            this.btnMassMove.TabIndex = 0;
+            this.btnMassMove.Text = "MassMove";
+            this.btnMassMove.UseVisualStyleBackColor = true;
+            this.btnMassMove.Click += new System.EventHandler(this.btnMassMove_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(33, 28);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(287, 31);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Contador de pruebas";
+            // 
+            // btnWarn
+            // 
+            this.btnWarn.Location = new System.Drawing.Point(38, 206);
+            this.btnWarn.Name = "btnWarn";
+            this.btnWarn.Size = new System.Drawing.Size(75, 23);
+            this.btnWarn.TabIndex = 2;
+            this.btnWarn.Text = "Warn";
+            this.btnWarn.UseVisualStyleBackColor = true;
+            this.btnWarn.Click += new System.EventHandler(this.btnWarn_Click);
+            // 
+            // btnMute
+            // 
+            this.btnMute.Location = new System.Drawing.Point(146, 206);
+            this.btnMute.Name = "btnMute";
+            this.btnMute.Size = new System.Drawing.Size(75, 23);
+            this.btnMute.TabIndex = 3;
+            this.btnMute.Text = "Mute";
+            this.btnMute.UseVisualStyleBackColor = true;
+            this.btnMute.Click += new System.EventHandler(this.btnMute_Click);
+            // 
+            // btnKick
+            // 
+            this.btnKick.Location = new System.Drawing.Point(38, 252);
+            this.btnKick.Name = "btnKick";
+            this.btnKick.Size = new System.Drawing.Size(75, 23);
+            this.btnKick.TabIndex = 4;
+            this.btnKick.Text = "Kick";
+            this.btnKick.UseVisualStyleBackColor = true;
+            this.btnKick.Click += new System.EventHandler(this.btnKick_Click);
+            // 
+            // btnBan
+            // 
+            this.btnBan.Location = new System.Drawing.Point(251, 252);
+            this.btnBan.Name = "btnBan";
+            this.btnBan.Size = new System.Drawing.Size(75, 23);
+            this.btnBan.TabIndex = 5;
+            this.btnBan.Text = "Ban";
+            this.btnBan.UseVisualStyleBackColor = true;
+            this.btnBan.Click += new System.EventHandler(this.btnBan_Click);
+            // 
+            // btnSS
+            // 
+            this.btnSS.Location = new System.Drawing.Point(146, 160);
+            this.btnSS.Name = "btnSS";
+            this.btnSS.Size = new System.Drawing.Size(75, 23);
+            this.btnSS.TabIndex = 6;
+            this.btnSS.Text = "Hack in SS";
+            this.btnSS.UseVisualStyleBackColor = true;
+            this.btnSS.Click += new System.EventHandler(this.btnSS_Click);
+            // 
+            // btnUnban
+            // 
+            this.btnUnban.Location = new System.Drawing.Point(146, 252);
+            this.btnUnban.Name = "btnUnban";
+            this.btnUnban.Size = new System.Drawing.Size(75, 23);
+            this.btnUnban.TabIndex = 7;
+            this.btnUnban.Text = "Unban";
+            this.btnUnban.UseVisualStyleBackColor = true;
+            this.btnUnban.Click += new System.EventHandler(this.btnUnban_Click);
+            // 
+            // lblAgregar
+            // 
+            this.lblAgregar.AutoSize = true;
+            this.lblAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAgregar.Location = new System.Drawing.Point(12, 127);
+            this.lblAgregar.Name = "lblAgregar";
+            this.lblAgregar.Size = new System.Drawing.Size(146, 18);
+            this.lblAgregar.TabIndex = 8;
+            this.lblAgregar.Text = "Agregar evidencia:";
+            // 
+            // txtContador
+            // 
+            this.txtContador.Location = new System.Drawing.Point(38, 85);
+            this.txtContador.Name = "txtContador";
+            this.txtContador.ReadOnly = true;
+            this.txtContador.Size = new System.Drawing.Size(288, 20);
+            this.txtContador.TabIndex = 9;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(355, 88);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(0, 16);
+            this.lblID.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(372, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Iniciar contador en:";
+            // 
+            // txtInicializador
+            // 
+            this.txtInicializador.Location = new System.Drawing.Point(531, 38);
+            this.txtInicializador.Name = "txtInicializador";
+            this.txtInicializador.Size = new System.Drawing.Size(100, 20);
+            this.txtInicializador.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(553, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "(Opcional)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(550, 276);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "By MrJosue#2114";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Ndeah re útil ah";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(546, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(568, 250);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 17;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnTp
+            // 
+            this.btnTp.Location = new System.Drawing.Point(251, 205);
+            this.btnTp.Name = "btnTp";
+            this.btnTp.Size = new System.Drawing.Size(75, 23);
+            this.btnTp.TabIndex = 18;
+            this.btnTp.Text = "Tp";
+            this.btnTp.UseVisualStyleBackColor = true;
+            this.btnTp.Click += new System.EventHandler(this.btnTp_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(248, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(227, 16);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "<-- Estos agregan 2 evidencias!";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(338, 233);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(219, 16);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "<-- Estos agregan 1 evidencia!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 298);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnTp);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtInicializador);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txtContador);
+            this.Controls.Add(this.lblAgregar);
+            this.Controls.Add(this.btnUnban);
+            this.Controls.Add(this.btnSS);
+            this.Controls.Add(this.btnBan);
+            this.Controls.Add(this.btnKick);
+            this.Controls.Add(this.btnMute);
+            this.Controls.Add(this.btnWarn);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnMassMove);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contador de pruebas HolyHCF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMassMove;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnWarn;
+        private System.Windows.Forms.Button btnMute;
+        private System.Windows.Forms.Button btnKick;
+        private System.Windows.Forms.Button btnBan;
+        private System.Windows.Forms.Button btnSS;
+        private System.Windows.Forms.Button btnUnban;
+        private System.Windows.Forms.Label lblAgregar;
+        private System.Windows.Forms.TextBox txtContador;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInicializador;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnTp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contador
+{
+    public partial class Form1 : Form
+    {
+        int evidences = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMassMove_Click(object sender, EventArgs e)
+        {
+            evidences = evidences + 2;
+            lblID.Text = "+2 evidencias!";
+            txtContador.Text = evidences.ToString();
+        }
+
+        private void btnSS_Click(object sender, EventArgs e)
+        {
+            evidences = evidences + 2;
+            lblID.Text = "+2 evidencias!";
+            txtContador.Text = evidences.ToString();
+        }
+
+        private void btnWarn_Click(object sender, EventArgs e)
+        {
+            evidences = evidences + 1;
+            lblID.Text = "+1 evidencia!";
+            txtContador.Text = evidences.ToString();
+        }
+
+        private void btnMute_Click(object sender, EventArgs e)
+        {
+            evidences = evidences + 1;
+            lblID.Text = "+1 evidencia!";
+            txtContador.Text = evidences.ToString();
+        }
+
+        private void btnKick_Click(object sender, EventArgs e)
+        {
+            evidences = evidences + 1;
+            lblID.Text = "+1 evidencia!";
+            txtContador.Text = evidences.ToString();
+        }
+
+        private void btnUnban_Click(object sender, EventArgs e)
+        {
+            evidences = evidences + 1;
+            lblID.Text = "+1 evidencia!";
+            txtContador.Text = evidences.ToString();
+        }
+
+        private void btnBan_Click(object sender, EventArgs e)
+        {
+            evidences = evidences + 1;
+            lblID.Text = "+1 evidencia!";
+            txtContador.Text = evidences.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            evidences = Convert.ToInt32(txtInicializador.Text);
+            txtContador.Text = evidences.ToString();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnTp_Click(object sender, EventArgs e)
+        {
+            evidences = evidences + 1;
+            lblID.Text = "+1 evidencia!";
+            txtContador.Text = evidences.ToString();
+        }
+    }
+}
